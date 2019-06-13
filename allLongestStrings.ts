0a7b8a239b271b54442bec88588586da7a78c951
Nataliya Kuziv <41294836+NatiKuz@users.noreply.github.com>
2019-06-13 22:25:17 +0300

    Update allLongestStrings.ts

--- allLongestStrings.ts
+++ allLongestStrings.ts
@@ -1,7 +1,7 @@
 // Given an array of strings, return another array containing all of its longest strings.
 function allLongestStrings(inputArray: string[]): string[] {
     let longestLength = 0;
-    const longestWords = string[];
+    const longestWords: string[] = [];
 
     inputArray.forEach((word: string) => {
         longestLength = longestLength < word.length ? word.length : longestLength;
@@ -9,11 +9,11 @@ function allLongestStrings(inputArray: string[]): string[] {
 
     inputArray.forEach((word: string) => {
         if(word.length === longestLength) {
-            longestWords.puch(word);
+            longestWords.push(word);
         }
     });
 
     return longestWords;
 }
 
-console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
+console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));    // [ 'aba', 'vcd', 'aba' ]
